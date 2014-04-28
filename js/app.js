1ddfa0738009a4e6f03f8c3677adaa86078950de
eliaqui <liam.quinlan@ericsson.com>
2014-04-28 20:10:11 +0100

    refactoring app.js file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+
     var products = [];
     var input_fields_valid = false;
 
@@ -71,13 +73,17 @@ $(document).ready(function() {
 
     function renderProductDataAndDesigns(product) {
         var $step3 = $("#designs");
-        var data_fields = "<div id=" + product.id + " class='row'><div class='col-xs-1'></div><div class='col-xs-4'><div class='thumbnail'><div class='input-group'><input id='location' type='text' class='form-control' placeholder='Window name/location'><span class='input-group-addon'></span></div><div class='input-group'><input id='width' type='text' class='form-control' placeholder='Width'><span class='input-group-addon'>milimeters</span></div><div class='input-group'><input id='height' type='text' class='form-control' placeholder='Height'><span class='input-group-addon'>milimeters</span></div><div class='input-group'><input id='color' type='text' class='form-control' placeholder='Color'><span class='input-group-addon'></span></div></div></div>";
+        var data_fields = "<div id=" + product.id + " class='row'><div class='col-xs-1'></div><div class='col-xs-4'><div class='thumbnail'><div class='input-group'><input id='location' type='text' class='form-control' placeholder='Window name/location'><span class='input-group-addon'></span></div><div class='input-group'><input id='width' type='text' class='form-control' placeholder='Width'><span class='input-group-addon'>milimeters</span></div><div class='input-group'><input id='height' type='text' class='form-control' placeholder='Height'><span class='input-group-addon'>milimeters</span></div><div class='btn-group btn-input clearfix'><button type='button' class='btn btn-default dropdown-toggle form-control' data-toggle='dropdown'><span data-bind='label'>White</span><span class='caret'></span></button><ul class='dropdown-menu' role='menu'><li><a href='#'>White</a></li><li><a href='#'>Cream</a></li><li><a href='#'>Woodgrain</a></li></ul></div></div></div>";
         var str = "<div class='col-xs-4'><div class='thumbnail'><img id='step_3_selected_design' src='images/" + product.design + ".png'></div></div></div>";
         var data_plus_str = data_fields + str;
         var html = $.parseHTML(data_plus_str);
         $step3.append(html);
-        var location_input = '#' + product.id + ' #location';
-        $(location_input).val(product.location);
+        var selector_location = '#' + product.id + ' #location';
+        $(selector_location).val(product.location);
+        var selector_width = '#' + product.id + ' #width';
+        $(selector_width).val(product.width);
+        var selector_height = '#' + product.id + ' #height';
+        $(selector_height).val(product.height);
     }
 
     $("#step_1_change_selection").hide();
@@ -139,6 +145,15 @@ $(document).ready(function() {
             var product_index = updateModel.call(this);
             validateFields(products[product_index]);
 
+        });
+
+        $('.dropdown-menu li').on('click', function(event) {
+            var $target = $(event.currentTarget);
+            var btn_group = $target.closest('.btn-group');
+            btn_group.find('[data-bind="label"]').text($target.text());
+            var dropdown_toggle = $target.children('.dropdown-toggle');
+            dropdown_toggle.dropdown('toggle');
+            return false;
         });
 
         if (products.length === 0) {
